3497832b5fd6cdffd0f7698df60ca48824b6af50
Kellye Greene <kellyecodes@gmail.com>
2018-03-24 15:56:45 -0400

    Commits website project to github

---
+++ mailer/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$my_email = 'hello@eclectusinteractive.com';
+
+$SMTP = array(
+    'enabled' => true,
+    'host' => 'smtp.gmail.com',
+    'username' => 'kellyecodes@gmail.com',
+    'password' => 'Mojojojo11',
+    'port' => 465,
+    'encryption' => 'ssl'
+);
+
+// Available colors: blue, green, grey, red and yellow
+$template_color = 'grey';
